58736fa5ac439ee9a3a0fcfc44722641d4b14ef1
James Woolfenden <James.Woolfenden@gmail.com>
2021-03-30 17:40:36 +0100

    compliance

--- examples/iam_policy/data.aws_iam_policy_document.heredoc.tf
+++ examples/iam_policy/data.aws_iam_policy_document.heredoc.tf
@@ -1,4 +1,7 @@
 data "aws_iam_policy_document" "heredoc" {
+	# checkov:skip=CKV_AWS_109: Simplified example
+	# checkov:skip=CKV_AWS_111: Simplified example
+	# checkov:skip=CKV_AWS_107: Simplified example
   statement {
     actions = [
       "ec2:*",
